f8780e1d5d33a5a3ad0c772b52882d8b5c01d320
Nahbot <andriandavamaulana5@gmail.com>
2024-04-17 14:14:54 +0700

    first commit

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export function Footer() {
+  return (
+    <footer className="text-secondary-foreground bg-background/50 h-8 w-full text-center">
+      <div className="border-input flex items-center justify-between border-t px-4 py-2">
+        <div aria-label="Disclaimer" className="text-sm font-semibold">
+          andzz
+        </div>
+    </footer>
+  );
+}
